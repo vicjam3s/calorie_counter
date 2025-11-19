0e4d3135ed515deefbb73bc3f9af16dc184aab3d
vicjam3s <njiruv.vn@gmail.com>
2025-11-19 18:51:13 +0300

    js contants

---
+++ script.js
@@ -0,0 +1,7 @@
+const foodForm = document.getElementById("foodForm");
+const foodNameInput = document.getElementById("foodName");
+const foodCaloriesInput = document.getElementById("foodCalories");
+const foodList = document.getElementById("foodList");
+const totalCaloriesDisplay = document.getElementById("totalCalories");
+const resetBtn = document.getElementById("resetBtn");
+const darkModeToggle = document.getElementById("darkModeToggle");
